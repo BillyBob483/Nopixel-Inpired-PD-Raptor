def4a8fb29bc8e0414587be1d07401c9ca24fb05
BillyBob483 <111546475+BillyBob483@users.noreply.github.com>
2022-10-31 04:10:43 -0500

    Add files via upload

---
+++ Raptor-PD/fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'bodacious'
+
+game 'gta5'
+
+
+files {
+
+	'data/**/carcols.meta',
+	'data/**/carvariations.meta',
+	'data/**/handling.meta',
+	'data/**/vehicles.meta',
+}
+
+	data_file 'HANDLING_FILE'			'data/**/handling.meta'
+	data_file 'VEHICLE_METADATA_FILE'	'data/**/vehicles.meta'
+	data_file 'CARCOLS_FILE'			'data/**/carcols.meta'
+	data_file 'VEHICLE_VARIATION_FILE'	'data/**/carvariations.meta'
+
